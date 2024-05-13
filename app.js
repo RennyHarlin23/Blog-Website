0b11dd00f147403c49116fa7cfb5bc5311ba0521
renny <rennydan72@gmail.com>
2024-05-13 10:35:32 +0530

    test

--- app.js
+++ app.js
@@ -12,18 +12,25 @@ app.use(express.static('public'))
 const database = new Datastore({filename: 'database.db'});
 database.loadDatabase();
 
+const newdb = new Datastore({filename: 'login.db'});
+newdb.loadDatabase();
+
 app.listen(3000, () => console.log("Server started in port 3000"));
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/blog', async (req, res) => {
     const docs = await database.findAsync({});
-    res.render('home',{arr: docs});
+    res.render('blog',{arr: docs});
 })
 
-app.get('/create', (req, res) => {
-    res.render('create');
+app.get('/create/:id', (req, res) => {
+    res.render('create',{id: req.params.id});
 })
 
-app.post('/create', (req, res) => {
+app.post('/create/:id', async(req, res) => {
     const currentDate = new Date();
 
     const body = {
@@ -31,20 +38,52 @@ app.post('/create', (req, res) => {
         post: req.body.blog,
         date: currentDate.toDateString()
     }
-    database.insert(body);
-    res.redirect('/');
+    await newdb.updateAsync({_id: req.params.id}, {$push: {docs: body}})
+    const found = await newdb.findAsync({_id: req.params.id});
+    console.log(found[0]);
+    res.render('blog',{arr: found[0].docs, id: req.params.id});
 })
 
 app.get('/posts/:id',async (req, res)=>{
-    const arr = await database.findAsync({});
-    arr.forEach(item => {
-        if(item._id === req.params.id){
-        res.render("post", {item: item});
-        }
-    })
+    const item = await database.findAsync({_id: req.params.id});
+    res.render('post', {item: item[0]});
 })
 
 app.get('/delete/:id', async (req, res)=>{
     const numRemoved = await database.removeAsync({_id: req.params.id}, {});
     res.redirect('/');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+    const found = await newdb.findAsync({username: req.body.username});
+    console.log(found);
+    if(found.length === 0){
+        res.redirect('/register');
+    }
+    else{
+        if(req.body.password === found[0].password){
+            res.render('blog',{arr: found[0].docs, id: found[0]._id});
+        }
+        else{
+            res.redirect('/login');
+        }
+    }
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', (req, res) => {
+    const entry = {
+        username: req.body.username,
+        password: req.body.password,
+        docs:[],
+    }
+    newdb.insert(entry);
+    res.redirect('/login');
 })
